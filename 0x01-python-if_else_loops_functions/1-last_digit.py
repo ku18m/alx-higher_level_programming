00dcefe4e6b613012f58957236d5b1fe22be34f5
Mohamed Kamal <ku18mz@gmail.com>
2023-10-03 19:37:08 +0300

    refactor task 1 script using abs && remainder

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-number = str(number)
-last_digit = int(number[-1])
-if number[0] == "-":
+last_digit = abs(number) % 10
+if number < 0:
     last_digit *= -1
-number = int(number)
 if last_digit > 5:
     print(f"Last digit of {number:d} is {last_digit:d} and is greater than 5")
 elif last_digit == 0:
