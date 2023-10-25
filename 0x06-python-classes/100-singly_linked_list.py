ded2c3fcd482280ec8fb3fcfae87dfe129796f1c
Mohamed Kamal <ku18mz@gmail.com>
2023-10-25 04:12:56 +0300

    try for check 10

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -10,8 +10,8 @@ class Node:
             data: The new node data.
             next_node: The next node.
         """
-        self._data = data
-        self._next_node = next_node
+        self.data = data
+        self.next_node = next_node
 
     @property
     def data(self):
@@ -61,14 +61,14 @@ class SinglyLinkedList:
             value: The data of the new node.
         """
         new = Node(value)
-        if self._head is None:
+        if self.__head is None:
             new.next_node = None
-            self._head = new
-        elif self._head.data > value:
-            new.next_node = self._head
-            self._head = new
+            self.__head = new
+        elif self.__head.data > value:
+            new.next_node = self.__head
+            self.__head = new
         else:
-            tmp = self._head
+            tmp = self.__head
             while (tmp.next_node is not None and
                     tmp.next_node.data < value):
                 tmp = tmp.next_node
@@ -77,9 +77,9 @@ class SinglyLinkedList:
 
     def __str__(self):
         """Prints the linked list"""
-        data = []
+        datas = []
         tmp = self._head
         while tmp is not None:
-            data.append(str(tmp.data))
+            datas.append(str(tmp.data))
             tmp = tmp.next_node
-        return ('\n'.join(data))
+        return ('\n'.join(datas))
