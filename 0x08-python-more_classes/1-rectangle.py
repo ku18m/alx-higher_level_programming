41982eb6c6da7ccf0aa4f1cc5dd6c34e5b7526bf
Mohamed Kamal <ku18mz@gmail.com>
2023-10-30 22:02:16 +0200

    remove else, guessing check 11

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -31,8 +31,7 @@ class Rectangle:
             raise TypeError("width must be an integer")
         elif value < 0:
             raise ValueError("width must be >= 0")
-        else:
-            self._width = value
+        self._width = value
 
     @property
     def height(self):
@@ -52,5 +51,4 @@ class Rectangle:
             raise TypeError("height must be an integer")
         elif value < 0:
             raise ValueError("height must be >= 0")
-        else:
-            self._height = value
+        self._height = value
