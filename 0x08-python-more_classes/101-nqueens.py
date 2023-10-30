74836fccb1759c14fb13906746b4e5bc001d5327
Mohamed Kamal <ku18mz@gmail.com>
2023-10-31 01:45:42 +0200

    comment every function

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -1,7 +1,23 @@
 #!/usr/bin/python3
 
 """
-Solves the N queens problem
+Solves the N queens problem.
+
+Determines all possible solutions to placing N
+N non-attacking queens on an NxN chessboard.
+
+Example:
+    $ ./101-nqueens.py N
+
+N must be an integer greater than or equal to 4.
+
+Attributes:
+    board (list): A list of lists representing the chessboard.
+    solutions (list): A list of lists containing solutions.
+
+Solutions are represented in the format [[r, c], [r, c], [r, c], [r, c]]
+where `r` and `c` represent the row and column, respectively, where a
+queen must be placed on the chessboard.
 """
 
 import sys
@@ -22,14 +38,21 @@ if __name__ == "__main__":
         solutions.append([i, None])
 
     def isqueen(y):
-        """Check if it's a queen"""
+        """Check if it's a queen.
+        Args:
+            y: The position to check.
+        """
         for x in range(n):
             if y == solutions[x][1]:
                 return True
         return False
 
     def is_solution(x, y):
-        """Checks if it's a solution or not"""
+        """Checks if it's a solution or not.
+        Args:
+            x: The position to check.
+            y: The position to check.
+        """
         if (isqueen(y)):
             return False
         i = 0
@@ -40,12 +63,18 @@ if __name__ == "__main__":
         return True
 
     def del_solution(x):
-        """Deletes the wrong solutions"""
+        """Deletes the wrong solutions.
+        Args:
+            x: The position to delete.
+        """
         for i in range(x, n):
             solutions[i][1] = None
 
     def find_solution(x):
-        """Finds the solution"""
+        """Finds the solution
+        Args:
+            x: The position to start.
+        """
         for y in range(n):
             del_solution(x)
             if is_solution(x, y):
