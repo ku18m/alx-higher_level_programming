23da1b6bc7f48ada08674c3ac684fe696a439e02
Mohamed Kamal <ku18mz@gmail.com>
2023-11-13 22:13:10 +0200

    add size property

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -29,6 +29,25 @@ class Square(Rectangle):
 
     def __str__(self):
         """Overriding __str__ method."""
-        return "[Square] ({}) {}/{} - {}".format(
-            self.id, self.x, self.y, self.height
-        )
+        return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y,
+                                                 self.height)
+
+    @property
+    def size(self):
+        """Private attribute property."""
+        return self.height
+
+    @size.setter
+    def size(self, value):
+        """
+        Private attribute setter.
+
+        Args:
+            value: The attribute new value.
+
+        Raises:
+            TypeError: If the value is not int.
+            ValueError: If the attribute is less than or equal zero.
+        """
+        self.width = value
+        self.height = value
