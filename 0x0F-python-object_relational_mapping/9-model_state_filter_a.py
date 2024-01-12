8de86815668928db2de1692646e911227673a042
Mohamed Kamal <ku18mz@gmail.com>
2024-01-12 12:09:08 +0200

    increase readability

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -19,7 +19,8 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
     session = Session()
     states = session.query(State).filter(
-        State.name.like(r"%a%")).order_by(State.id)
+        State.name.like(r"%a%")
+        ).order_by(State.id)
     for state in states:
         print(state)
     session.close()
