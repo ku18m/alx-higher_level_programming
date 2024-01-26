23333300f33bfb756e66760df5ee8666adf75c64
Mohamed Kamal <ku18mz@gmail.com>
2024-01-26 03:39:42 +0200

    Changing var name

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Takes in a URL, sends a request to that URL, and displays the body of the response.
-curl -sH "X-HolbertonSchool-User-Id: 98" "$1"
+curl -sH "X-School-User-Id: 98" "$1"
