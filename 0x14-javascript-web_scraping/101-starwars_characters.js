87a21094a1cdba920483802a9e9bad13627d4ebb
Mohamed Kamal <ku18mz@gmail.com>
2024-02-21 00:15:43 +0200

    script 8, creating a promise just to keep using request module, many modules solve it by have builtin promise in thier request method but it's the task requirements.

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -2,19 +2,46 @@
 
 const request = require('request');
 
-const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
-request(url, (error, response, body) => {
-  if (error) {
-    console.log(error);
-  } else if (response.statusCode === 200) {
-    JSON.parse(body).characters.forEach((character) => {
-      request(character, (error, response, body) => {
+function fetchCharacter (characterUrl) {
+  return new Promise((resolve, reject) => {
+    request(characterUrl, (error, response, body) => {
+      if (error) {
+        reject(error);
+      } else if (response.statusCode === 200) {
+        resolve(JSON.parse(body).name);
+      } else {
+        reject(new Error(`Failed to fetch character: ${response.statusCode}`));
+      }
+    });
+  });
+}
+
+(async function () {
+  const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
+  try {
+    const filmResponse = await new Promise((resolve, reject) => {
+      request(url, (error, response, body) => {
         if (error) {
-          console.log(error);
+          reject(error);
         } else if (response.statusCode === 200) {
-          console.log(JSON.parse(body).name);
+          resolve(body);
+        } else {
+          reject(new Error(`Failed to fetch film: ${response.statusCode}`));
         }
       });
     });
+
+    const filmData = JSON.parse(filmResponse);
+    const characters = filmData.characters;
+    for (const characterUrl of characters) {
+      try {
+        const characterName = await fetchCharacter(characterUrl);
+        console.log(characterName);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  } catch (error) {
+    console.error(error);
   }
-});
+})();
